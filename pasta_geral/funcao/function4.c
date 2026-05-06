8f3e0124d0ccbb48911fcf75e088859e957fd4b3
Miguel2 <miguel.v.santos@ba.estudante.senai.br>
2026-05-06 19:08:39 +0000

    save[

--- pasta_geral/funcao/function4.c
+++ pasta_geral/funcao/function4.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+float soma_total=0;
+float n1, n2;
+
 void limpar_tela() {
     #ifdef _WIN32
         system("cls");
@@ -10,8 +13,18 @@ void limpar_tela() {
 }
 void  aguardar_enter     ();
 void  limpar_buffer      ();
+
+void calculo(){
+     printf("\nDigite o primeiro numero: ");
+    scanf("%f", &n1);
+    soma_total+=n1;
+    printf("\nDigite o segundo numero: ");
+    scanf("%f", &n2);
+    soma_total+=n2;
+    
+}
 int subtrair(float n1, float n2){
-    return n2-n1;
+    return n1-n2;
 }
 int somar(float n1, float n2){
     return n1+n2;
@@ -23,18 +36,11 @@ float divisao(float n1, float n2){
     return n2/n1;
 }
 int main(){
-    int n1, n2;
-    int soma, soma_total, sub, op;
-    float mult, div;
 
+    float soma, sub, mult, div;
+    int op;
     do{
-
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &n1);
-    soma_total+=n1;
-    printf("\nDigite o segundo numero: ");
-    scanf("%d", &n2);
-    soma_total+=n2;
+   
     limpar_tela();
     printf("\n1- Somar os valores: ");
     printf("\n2- Subtrair os valores: ");
@@ -42,33 +48,60 @@ int main(){
     printf("\n4- Dividir os valores: ");
     printf("\n0- Sair ");
      printf("\nDigite a opcao desejada: ");
-     scanf("%d", op);
+     scanf("%d", &op);
 
          switch (op)
         {
         case 1:
+        calculo();
         soma = somar(n1, n2);
-        printf("\nA soma dos numeros digitados é: %d", soma);
+        printf("\nA soma dos numeros digitados é: %.2f", soma);
+        printf("\nA soma total dos valores é de: %.2f", soma_total);
+        aguardar_enter();
         limpar_buffer();
         break;
-    
+        
         case 2:
+        calculo();
         sub = subtrair(n1, n2);
-        printf("\nA subtração dos numeros digitados é: %d", sub);
+        printf("\nA subtração dos numeros digitados é: %.0f", sub);
+        aguardar_enter();
+        limpar_buffer();
+        break;
+        
+        case 3:
+        calculo();
+        mult = multiplicacao(n1, n2);
+        printf("\nA multiplicacao dos numeros digitados é: %.2f", mult);
+        aguardar_enter();
+        limpar_buffer();
+        break;
+        
+        case 4:
+        calculo();
+        if (n2==0){
+            printf("\nERRO!\nNumero Negativo Inserido");
+        }else{
+
+            div = divisao(n1, n2);
+            printf("\nA divisao dos numeros digitados é: %f", div);
+        }
+        aguardar_enter();
         limpar_buffer();
         break;
 
-        case 3:
-        mult = multiplicacao(n1, n2);
-        printf("\nA multiplicacao dos numeros digitados é: %.2f", mult);
+        case 0:
+        printf("Saindo...");
+        aguardar_enter();
+        limpar_buffer();
         break;
-     default:
+
+        default:
+        printf("Valor digitado invalido.\nTente novamente");
+        aguardar_enter();
+        limpar_buffer();
         break;
      }
-    
-    div = divisao(n1, n2);
-    
-    printf("\nA divisao dos numeros digitados é: %.2f", div);
     }while(op!=0);
     return 0;
 }
