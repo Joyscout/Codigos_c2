3a5ab1cefdb9f65b5cec19708a81dc4ff7896bd1
Miguel2 <miguel.v.santos@ba.estudante.senai.br>
2026-05-19 12:45:06 +0000

    save

--- pasta_geral/funcao/functionatv10.c
+++ pasta_geral/funcao/functionatv10.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void limpar_tela() {
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
-}
-void mults(int matrix1[2][3], int matrix2[3][4], int resultado[2][4]) {
+void ler(int linha, int con,int matrix1[linha][con] ) {
+    int i, j, k;
+
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < con; j++) {
+            printf("Digite o valor[%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+            }
+        }
+    }
+
+void mults(int matA[2][3], int matB[3][4], int matC[2][4] ){
     int i, j, k;
 
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 4; j++) {
-            resultado[i][j] = 0;
+            matC[i][j] = 0;
             for (k = 0; k < 3; k++) {
-                resultado[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
-        }
-    }
+                matC[i][j] += matA[i][k] * matB[k][j];
+}
+}
+}
 }
 int main(){
-    int matrix1[2][3]={{2,4,6},{8,10,12}}, matrix2[3][4]={{3,6,9,12},{15,18,21},{24,27,30,33}};
-        int i,j;
-        int resultado[2][4];
-
-
-        mults(matrix1,matrix2,resultado);
-
+    int i, j;
+    int matA[2][3];
+    int matB[3][4];
+    int matC[2][4];
+        
+        printf("Matriz A: \n");
+        ler(2, 3, matA);
+        printf("\nMatriz B: \n");
+        ler(3, 4, matA);
+       mults(matA, matB, matC);
         for(i=0; i<2; i++){
-            for(j=0; j<2; j++){
-                printf("%d\t", resultado[i][j]);
+            for(j=0; j<4; j++){
+                printf("%d\t", matC[i][j]);
             }
             printf("\n");
         }
